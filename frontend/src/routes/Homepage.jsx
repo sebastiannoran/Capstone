032d95b545cef1e264d717dd9aa29ae643d32051
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-27 13:31:55 -0400

    Login and Color changes incopoprated

--- frontend/src/routes/Homepage.jsx
+++ frontend/src/routes/Homepage.jsx
@@ -1,9 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
 const Homepage = () => {
   return (
-    <div className="max-w-screen-2xl mx-auto">
-      <p className="text-center mt-12 text-5xl">Welcome</p>
-      <div className="mt-12">
-        <p className="text-center">Search Bar</p>
+    <div className="bg-[#FFFDED] h-screen flex flex-col justify-center items-center">
+      <p className="text-5xl mb-8">Welcome</p>
+      <div className="flex gap-4">
+        <Link to = '/Login'>
+      <button className="bg-[#D1D5B5] text-black px-4 py-2 rounded">Login</button> {/*Route to Login.jsx*/}
+      </Link>
+      <Link to = '/Register'>
+      <button className="bg-[#D1D5B5] text-black px-4 py-2 rounded">Register</button> {/*Route to Register.jsx */}
+      </Link>
       </div>
     </div>
   );
