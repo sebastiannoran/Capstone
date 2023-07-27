131fe5fcfe05554e6e4d95e8914567130c5880a3
Nikko D <ndasalla1323@gmail.com>
2023-07-26 21:39:06 -0400

    added routes for root & homepage & college components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import "./App.css";
+import Root from "./routes/root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import College from "./routes/colleges/College";
+import Homepage from "./routes/Homepage";
 
-function App() {
-  return (
-    <>
-      <div>
-        <p>Hello World</p>
-      </div>
-    </>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "/college/:collegeId",
+        element: <College />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ frontendTest/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <nav></nav>
+      <p className="text-8xl text-red-500">Hello</p>
+      <p>Another</p>
+    </div>
+  );
+}
+
+export default App;
