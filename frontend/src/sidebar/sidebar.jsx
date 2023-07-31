b4d9f19ac7eea0d435b29a533c84dc249fa0f4e7
Junyao <junyaokh822@gmail.com>
2023-07-30 21:51:30 -0400

    adjust the size of the Faschool icon

--- frontend/src/sidebar/sidebar.jsx
+++ frontend/src/sidebar/sidebar.jsx
@@ -1,3 +1,171 @@
+// import React, { useState } from 'react';
+// import { AiFillCaretLeft } from 'react-icons/ai';
+// import { FaSchool } from 'react-icons/fa';
+// import { BiSearch } from 'react-icons/bi';
+// import { MdSchool } from 'react-icons/md';
+// import { BsChevronDown } from 'react-icons/bs';
+
+// import { useNavigate } from "react-router-dom";
+
+
+// const Sidebar = () => {
+
+//   const [open, setOpen] = useState(true);
+
+//   const [searchQuery, setSearchQuery] = useState('');
+
+//   const navigate = useNavigate();
+
+//   const menus = [
+//     {
+//       title: 'Biology',
+//       courseCode: 'BIO',
+//       submenu: true,
+//       submenuItems: [
+//         { title: 'BIO 103' },
+//         { title: 'BIO 106' },
+//         { title: 'BIO 150' }
+//       ]
+//     },
+//     {
+//       title: 'Computer Science',
+//       courseCode: 'CSC',
+//       submenu: true,
+//       submenuItems: [
+//         { title: 'CSC 126' },
+//         { title: 'CSC 211' },
+//         { title: 'CSC 326' }
+//       ]
+//     },
+//     {
+//       title: 'Chemistry',
+//       courseCode: 'CHM',
+//       submenu: true,
+//       submenuItems: [
+//         { title: 'CHM 100' },
+//         { title: 'CHM 141' },
+//         { title: 'CHM 142' }
+//       ]
+//     },
+//     {
+//       title: 'Accounting',
+//       courseCode: 'ACC',
+//       submenu: true,
+//       submenuItems: [
+//         { title: 'ACC 114' },
+//         { title: 'ACC 218' },
+//         { title: 'BUS 160' }
+//       ]
+//     }
+//   ];
+
+//   const [menusOpen, setMenusOpen] = useState(
+//     menus.reduce((acc, menu, index) => {
+//       acc[index] = menu.submenu;
+//       return acc;
+//     }, {})
+//   );
+
+//   const toggleMenu = (index) => {
+//     setMenusOpen({
+//       ...menusOpen, 
+//       [index]: !menusOpen[index]
+//     });
+//   }
+
+//   return (
+//     <div className="flex">
+
+//     <div className={`flex-shrink-0 bg-cyan-400 p-5 pt-8 ${open ? 'w-72' : 'w-20'} duration-300 relative`}>
+//       <div className={`max-h-screen ${open ? 'h-screen overflow-y-auto' : 'h-20'}  pr-5`}>
+
+//         <AiFillCaretLeft
+//           className={`bg-black text-white text-4xl absolute -right-8 top-1/2 transform -translate-y-1/2 border boarder-dark border-5 cursor-pointer ${!open && 'rotate-180'}`}
+//           onClick={() => setOpen(!open)} 
+//         />
+
+//         <div className="inline-flex">
+//           <FaSchool 
+//             className={`bg-emerald-200 text-5xl rounded cursor-pointer block float-left px-1 mr-6 duration-500 ${open && 'rotate-[360deg]'}`} 
+//           />
+//           <h1 className={`text-dark origin-left font-sm text-1xl duration-300 ${!open && 'scale-0'}`}>
+//             College of Staten Island
+//           </h1>
+//         </div>
+      
+//         <div className={`flex items-center rounded-md bg-white mt-5 ${open ? 'p-2 px-6' : ''}`}>
+//           <BiSearch
+//             className={`text-black text-lg block float-left cursor-pointer ${open ? 'mr-4' : 'mr-1'}`}
+//           />
+//           <input 
+//             type="search"
+//             placeholder="Search"
+//             value={searchQuery}
+//             onChange={(e) => setSearchQuery(e.target.value)}
+//             className={`text-base bg-transparent w-full text-black focus:outline-none ${!open && 'hidden'}`}
+//           />
+//         </div>
+
+//         <ul className="pt-2">
+//           {menus.map((menu, index) => (
+//             <>
+//               <li
+//                 key={index}
+//                 onClick={() => toggleMenu(index)}
+//                 className="text-black-300 text-lg flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-indigo-400 rounded-md mt-2"
+//               >
+//                 <span className="text-2xl block float-left">
+//                   <MdSchool className={`transition-transform ${!open && 'translate-x-[-12px]'}`}/>
+//                 </span>
+//                 <span className={`text-base font-medium flex-1 duration-200 ${!open && 'hidden'}`}>
+//                   {menu.title}
+//                 </span>
+//                 {menu.submenu && open && (
+//                   <BsChevronDown
+//                     className={`${menusOpen[index] && 'rotate-180'}`}
+//                   />
+//                 )}
+//               </li>
+
+//               {menu.submenu && menusOpen[index] && open && (
+//                 <ul className="bg-cyan-50 rounded-md mt-2">
+//                   {menu.submenuItems
+//                   .filter(
+//                     (submenuItem) =>
+//                       submenuItem.title.toLowerCase().includes(searchQuery.toLowerCase()) || // Check if title matches search query
+//                       submenuItem.title.toLowerCase().includes(menu.courseCode.toLowerCase() + ' ' + searchQuery.toLowerCase()) // Check if course code + title matches search query
+//                   )
+//                   .map(submenuItem => (
+//                     <li 
+//                       key={submenuItem.title}
+//                       className="text-black-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-7 hover:bg-indigo-400 rounded-md mt-2 "
+//                       onClick={() => navigate(`/college/${menu.courseCode}/${submenuItem.title}`)}
+//                     >
+//                       {submenuItem.title}
+//                     </li>
+//                   ))}
+//                 </ul>
+//               )}
+//             </>
+//           ))}
+//         </ul>
+
+//       </div>
+
+//       </div>
+
+
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
+
+
+
+
+
+
 import React, { useState } from 'react';
 import { AiFillCaretLeft } from 'react-icons/ai';
 import { FaSchool } from 'react-icons/fa';
@@ -84,19 +252,22 @@ const Sidebar = () => {
           onClick={() => setOpen(!open)} 
         />
 
-        <div className="inline-flex">
-          <FaSchool 
+      <div className="flex items-center">
+          <div className="flex-shrink-0">
+        <FaSchool 
             className={`bg-emerald-200 text-5xl rounded cursor-pointer block float-left px-1 mr-6 duration-500 ${open && 'rotate-[360deg]'}`} 
           />
-          <h1 className={`text-dark origin-left font-medium text-1xl duration-300 ${!open && 'scale-0'}`}>
-            College of Staten Island
-          </h1>
-        </div>
-      
+          </div>
+        <h1 className={`text-dark origin-left font-lg font-bold  text-2xl duration-300 ${!open && 'scale-0'}`}>
+          College of Staten Island
+        </h1>
+      </div>
+
         <div className={`flex items-center rounded-md bg-white mt-5 ${open ? 'p-2 px-6' : ''}`}>
           <BiSearch
             className={`text-black text-lg block float-left cursor-pointer ${open ? 'mr-4' : 'mr-1'}`}
           />
+          
           <input 
             type="search"
             placeholder="Search"
@@ -105,6 +276,7 @@ const Sidebar = () => {
             className={`text-base bg-transparent w-full text-black focus:outline-none ${!open && 'hidden'}`}
           />
         </div>
+        
 
         <ul className="pt-2">
           {menus.map((menu, index) => (
@@ -160,11 +332,3 @@ const Sidebar = () => {
 }
 
 export default Sidebar;
-
-
-
-
-
-
-
-
