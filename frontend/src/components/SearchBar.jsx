438876896473b089459cb7499fd3aaf90ea5462b
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-31 10:01:56 -0400

    Nikko+Micheal branch merged, homepage route+search

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,47 +1,43 @@
-import React from 'react';
-import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
-export const SearchBar = () => {
-    return (
-    <div
-      className="flex flex-col items-center space-y-4"
-      style={{ filter: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25))" }}
-    >
-      <div
-        className="w-[400px] h-[50px] relative"
-        style={{ filter: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25))" }}
-      >
-        <div className="w-[400px] h-[50px] absolute left-[-1px] top-[-1px] rounded-[60px] bg-white/[0.89] border-2 border-black" />
-        <FaSearch
-          className="w-[26.56px] h-[28.72px] absolute left-[19.55px] top-[10.14px] object-cover"
-        />
-        <p className="w-[308.94px] h-[29.79px] absolute left-[57.99px] top-[2.57px] text-2xl italic text-left text-[#b7b7b7]">
-          Type to Search...
-        </p>
-      </div>
-      <div className="flex gap-4">
-        {/* Login button */}
-        <button
-          className="bg-[#D1D5B5] text-black px-4 py-2 rounded"
-          onClick={() => {
-            <Link to = '/LoginPage'></Link>
-          }}
+export const SearchBar = ({ handleSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    handleSearch(query);
+  };
+
+  return (
+    <div className="flex flex-col items-center space-y-4">
+      <div className="relative">
+        {/* Search bar container */}
+        <div
+          className="w-[400px] h-[50px] relative"
+          style={{ filter: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25)), " }}
         >
-          Login
-        </button>
-        {/* Register button */}
-        <button
-          className="bg-[#D1D5B5] text-black px-4 py-2 rounded"
-          onClick={() => {
-            <Link to = '/Register'></Link>
-          }}
-        >
-          Register
-        </button>
+          <div className="w-[400px] h-[50px] absolute left-[-1px] top-[-1px] rounded-[60px] bg-white/[0.89] border-2 border-black" />
+          <FaSearch
+            className="w-[26.56px] h-[28.72px] absolute left-[19.55px] top-[10.14px] object-cover"
+          />
+          <input
+            type="text"
+            className="w-[308.94px] h-[29.79px] absolute left-[57.99px] top-[7.10px] bg-transparent text-lg italic text-left text-[#b7b7b7]"
+            placeholder="Type to Search..."
+            value={searchQuery}
+            onChange={handleInputChange}
+          />
+        </div>
+        {/* Search results container */}
+        <div className='SearchResults'>
+          {/* Display the search results here */}
+          {/* keep empty since no database exiists */}
+        </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
 
