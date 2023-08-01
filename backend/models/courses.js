f9a14bac2d0d7fdc74e837e9b3dd2a2b77239003
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-08-01 15:38:12 -0400

    1 course can have may posts relationship up

--- backend/models/courses.js
+++ backend/models/courses.js
@@ -10,6 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) { 
+      //1 course page has many posts
+      this.hasMany(models.posts);
     }
   }
   courses.init({
