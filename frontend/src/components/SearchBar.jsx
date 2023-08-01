999dfa62ce1f3d920c9a23c30f0e1648ccb57523
Junyao <junyaokh822@gmail.com>
2023-08-01 11:17:01 -0400

    edit changes

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,14 +1,59 @@
+// import React, { useState } from 'react';
+// import { FaSearch } from 'react-icons/fa';
+
+// export const SearchBar = ({ handleSearch }) => {
+//   const [searchQuery, setSearchQuery] = useState('');
+
+//   const handleInputChange = (event) => {
+//     const query = event.target.value;
+//     setSearchQuery(query);
+//     handleSearch(query);
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center space-y-4">
+//       <div className="relative">
+//         {/* Search bar container */}
+//         <div
+//           className="w-[400px] h-[50px] relative"
+//           style={{ filter: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25)), " }}
+//         >
+//           <div className="w-[400px] h-[50px] absolute left-[-1px] top-[-1px] rounded-[60px] bg-white/[0.89] border-2 border-black" />
+//           <FaSearch
+//             className="w-[26.56px] h-[28.72px] absolute left-[19.55px] top-[10.14px] object-cover"
+//           />
+//           <input
+//             type="text"
+//             className="w-[308.94px] h-[29.79px] absolute left-[57.99px] top-[7.10px] bg-transparent text-lg italic text-left text-black focus:outline-none mt-1"
+//             placeholder="Type to Search..."
+//             value={searchQuery}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         {/* Search results container */}
+//         <div className='SearchResults'>
+//           {/* Display the search results here */}
+//           {/* keep empty since no database exiists */}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+
+
+
+
 import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
-export const SearchBar = ({ handleSearch }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+export const SearchBar = () => {
+  const [input, setInput] = useState("");
 
-  const handleInputChange = (event) => {
-    const query = event.target.value;
-    setSearchQuery(query);
-    handleSearch(query);
-  };
+  const fetchdata = (value)=>{
+    fetch("")
+  }
 
   return (
     <div className="flex flex-col items-center space-y-4">
@@ -26,8 +71,8 @@ export const SearchBar = ({ handleSearch }) => {
             type="text"
             className="w-[308.94px] h-[29.79px] absolute left-[57.99px] top-[7.10px] bg-transparent text-lg italic text-left text-black focus:outline-none mt-1"
             placeholder="Type to Search..."
-            value={searchQuery}
-            onChange={handleInputChange}
+            value={input}
+            onChange={(e)=>setInput(e.target.value)}
           />
         </div>
         {/* Search results container */}
@@ -39,5 +84,3 @@ export const SearchBar = ({ handleSearch }) => {
     </div>
   );
 };
-
-
--- frontend/src/routes/colleges/College.jsx
+++ frontend/src/routes/colleges/College.jsx
@@ -1,28 +1,48 @@
 import React from "react";
-import { Link, useParams } from 'react-router-dom';
-import Sidebar from "../../sidebar/sidebar";
+import { Link, useParams,useLocation } from "react-router-dom";
+import Sidebar from "../../components/Sidebar";
 
 function College({ courses }) {
-  const { collegeId } = useParams();
 
+  const { collegeId } = useParams();
+  
+  const {search} = useLocation();
+const query = new URLSearchParams(search).get('query');
+
+let searchQuery = '';
+
+if (location.state) {
+  searchQuery = location.state.searchQuery; 
+}
   return (
     <div className="grid grid-cols-6">
       <Sidebar />
-      <div className="col-span-4 flex flex-col justify-center">
-        <div className="container m-auto h-screen grid grid-cols-3 gap-20 content-center place-items-center w-1/2">
-          {courses.map((course) => (
-            <Link to={`/college-home/${collegeId}/${course.id}`} key={course.id}>
-              <button className="w-40 h-32 p-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      
+      <div className="col-span-4 flex flex-col justify-center items-center">
+      
+        <h1 className="text-center mb-14 text-5xl font-bold mt-10">Welcome to <br/> College of Staten Island </h1>
+        
+          <h1 className="text-center mb-1 text-3xl text-red-600 font-bold">Popular Courses:</h1>
+          
+          <div className="container mt-[-9rem] m-auto h-screen grid grid-cols-3 gap-20 content-center place-items-center w-1/2">
+
+            {courses.map(course => (
+              <Link key={course.id} to={`/college-home/${collegeId}/${course.id}`}>
+                <button className="w-40 h-32 p-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 {course.courseCode}:
                 <br />{course.courseName}
               </button>
-            </Link>
-          ))}
-        </div>
+              </Link>
+            ))}
+
+          </div>
+        
+      
       </div>
 
     </div>
   );
+
 }
 
 export default College;
