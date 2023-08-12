e44e557f01896691b3a3b9d0c17f399bc6564025
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-08-12 12:40:57 -0400

    Brooklyn, Hunter, and Baruch majors added

--- backend/seeders/20230812144938-Brooklyn-majors.js
+++ backend/seeders/20230812144938-Brooklyn-majors.js
@@ -112,6 +112,6 @@ module.exports = {
    },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('majors', null, {});
+    await queryInterface.bulkDelete('majors', null, {})
   }
 };
